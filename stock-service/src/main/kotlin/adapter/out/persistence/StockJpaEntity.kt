e2e843ff5db6d05f3e3d27ccabdb1a11b4e6ac38
RefinedStone <chem.en.9273@gmail.com>
2024-05-31 03:05:16 +0900

    feat: jpaEntity

--- stock-service/src/main/kotlin/adapter/out/persistence/StockJpaEntity.kt
+++ stock-service/src/main/kotlin/adapter/out/persistence/StockJpaEntity.kt
@@ -1,2 +1,15 @@
 package com.example.stock.adapter.out.persistence
 
+import com.example.stock.domain.StockDerivative
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "stocks")
+class StockJpaEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    val name: String,
+    val price: Double,
+    val derivative: Double,
+)
