83a918f7e19c5e67616ed25191f8da1fdf4fadb1
RefinedStone <chem.en.9273@gmail.com>
2024-05-31 03:05:42 +0900

    feat: application Command

--- stock-service/src/main/kotlin/application/port/in/AssembleStockInfoCommand.kt
+++ stock-service/src/main/kotlin/application/port/in/AssembleStockInfoCommand.kt
@@ -1,2 +1,14 @@
 package com.example.stock.application.port.`in`
 
+class AssembleStockInfoCommand(
+    val stockId: String,
+    val stockName: String,
+    val stockPrice: Int,
+    val stockDerivative: Double,
+) {
+    init {
+        // TODO: stockId validation
+        require(stockName.isNotEmpty()) { "The stockName must be 1 characters at least" }
+        require(stockPrice > 0) { "The stockPrice must be greater than 0" }
+    }
+}
