87db1444caa227806c64bed9dc765564aeeb80e0
RefinedStone <chem.en.9273@gmail.com>
2024-05-31 03:05:08 +0900

    feat: controller

--- stock-service/src/main/kotlin/adapter/in/web/SketchController.kt
+++ stock-service/src/main/kotlin/adapter/in/web/SketchController.kt
@@ -1,14 +1,27 @@
 package com.example.stock.adapter.`in`.web
 
+import com.example.stock.common.WebAdapter
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
-@RestController
+@WebAdapter // 명시적 annotation
 class SketchController {
 
-    @GetMapping("/test")
+    @GetMapping("/stock/test")
     fun test(): String {
+        return "Hello Kotlin"
+    }
+
+    @PostMapping("/stock/assemble")
+    fun assembleStockInfo(@RequestBody request: AssembleStockInfoRequest): String {
         return "Hello Kotlin"
     }
 }
 
+data class AssembleStockInfoRequest(
+    val stockId: String,
+    val stockName: String,
+    val stockPrice: Int,
+    val stockDerivative: Double,
+)
