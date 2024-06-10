3bc597bc7d75559479c97813f16365edef3183f3
RefinedStone <chem.en.9273@gmail.com>
2024-06-10 11:30:00 +0900

    feat: cache test

--- src/main/kotlin/com/example/sketch/sketch/Sketch.kt
+++ src/main/kotlin/com/example/sketch/sketch/Sketch.kt
@@ -1,15 +1,32 @@
 package com.example.sketch.sketch
 
-import org.springframework.http.ResponseEntity
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class SketchController {
-
+class SketchController(
+    val cacheTestService: CacheTestService,
+) {
     @GetMapping("/test")
     fun test(): String {
         return "Hello Kotlin"
+    }
+
+    // 캐시 테스트
+    @Cacheable(cacheNames = ["authentication"], key = "'api_tokens'")
+    @GetMapping("/test/cache")
+    fun testCache(): String {
+        return cacheTestService.testCache()
+    }
+}
+
+@Service
+class CacheTestService {
+    fun testCache(): String {
+        println("you cant read this message if it would be cached")
+        return "Cache Test"
     }
 }
 
