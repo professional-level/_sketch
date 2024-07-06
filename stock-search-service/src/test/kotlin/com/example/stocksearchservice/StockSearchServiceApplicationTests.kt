ab5d5a5f86bdb7d9f581c461fa62689cbae9154a
RefinedStone <chem.en.9273@gmail.com>
2024-07-07 06:36:04 +0900

    feat: stock-search-service init

---
+++ stock-search-service/src/test/kotlin/com/example/stocksearchservice/StockSearchServiceApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.stocksearchservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockSearchServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
