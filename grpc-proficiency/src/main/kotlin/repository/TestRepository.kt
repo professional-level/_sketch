c79b82dc790420f175729d61e45d87cdfbdcd0aa
RefinedStone <chem.en.9273@gmail.com>
2024-07-31 04:20:34 +0900

    feat: TestRepository

---
+++ grpc-proficiency/src/main/kotlin/repository/TestRepository.kt
@@ -0,0 +1,59 @@
+package com.example.repository
+
+import com.example.entity.TestEntity
+import com.example.entity.TestEntity.Companion.generateDummyData
+import com.example.entity.TestEntity.Companion.protoToKotlin
+import example.Protobuff
+import org.springframework.stereotype.Component
+import java.util.concurrent.ConcurrentHashMap
+
+@Component
+object TestRepository {
+    private val repository: MutableMap<Int, TestEntity> = ConcurrentHashMap()
+    fun add(entity: TestEntity) {
+        repository[entity.id] = entity
+    }
+
+    fun findById(id: Int): TestEntity? {
+        return repository[id]
+    }
+
+    fun findAll(): Map<Int, TestEntity> {
+        return repository.toMap()
+    }
+
+    fun findOne(): TestEntity? {
+        return repository[0]
+    }
+
+    fun setDummy(size: Int) {
+        generateDummyData(testEntitySize = size, memberSize = 10, productSize = 10).map {
+            add(protoToKotlin(it))
+        }
+    }
+}
+
+@Component
+object TestProtobufRepository {
+    private val repository: MutableMap<Int, Protobuff.TestEntity> = ConcurrentHashMap()
+
+    fun add(entity: Protobuff.TestEntity) {
+        repository[entity.id] = entity
+    }
+
+    fun findById(id: Int): Protobuff.TestEntity? {
+        return repository[id]
+    }
+
+    fun findAll(): Map<Int, Protobuff.TestEntity> {
+        return repository.toMap()
+    }
+
+    fun findOne(): Protobuff.TestEntity? {
+        return repository[0]
+    }
+
+    fun setDummy(size: Int) {
+        generateDummyData(testEntitySize = size, memberSize = 10, productSize = 10).forEach { add(it) }
+    }
+}
