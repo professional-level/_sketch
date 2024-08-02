9d343f5f83d35432d4cc262e216cc507aedbf047
RefinedStone <chem.en.9273@gmail.com>
2024-08-02 14:00:05 +0900

    settings: DB settings

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -17,6 +17,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
 //    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(kotlin("test"))
+    // reactive
+    implementation("io.smallrye.reactive:mutiny-kotlin:2.3.0")
+    // Hibernate Reactive
+    implementation("org.hibernate.reactive:hibernate-reactive-core:2.3.0.Final")
+//    implementation("io.vertx:vertx-jdbc-client:4.5.7")
+//    implementation("io.agroal:agroal-pool:2.3")
+    // jakarta inject
+    implementation("jakarta.inject:jakarta.inject-api:2.0.1")
 }
 
 tasks.test {
