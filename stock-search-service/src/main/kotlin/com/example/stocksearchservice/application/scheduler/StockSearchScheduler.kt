d64248a93331228be565bac4d17cb2723bcd846e
RefinedStone <chem.en.9273@gmail.com>
2024-08-05 07:12:10 +0900

    feat: scheduler

--- stock-search-service/src/main/kotlin/com/example/stocksearchservice/application/scheduler/StockSearchScheduler.kt
+++ stock-search-service/src/main/kotlin/com/example/stocksearchservice/application/scheduler/StockSearchScheduler.kt
@@ -1,14 +1,27 @@
 package com.example.stocksearchservice.application.scheduler
 
+import com.example.stocksearchservice.domain.StockLog
 import com.example.stocksearchservice.domain.repository.StockInformationRepository
+import kotlinx.coroutines.CoroutineScope
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.time.ZonedDateTime
 
 @Component
 class StockSearchScheduler(
     private val stockInformationRepository: StockInformationRepository,
 ) {
+    // example of cron = "초 분 시간-시간 ? * 요일-요일"
+    @Scheduled(cron = "15 */2 9-18 ? * MON-FRI ")
+    fun sample() {
+        val time = ZonedDateTime.now()
+        println("[${Thread.currentThread()}]:$time")
+    }
 
-    fun temp1() {
-        stockInformationRepository.findTop10VolumeStocks()
+    @Scheduled(cron = "15 */2 9-18 ? * MON-FRI ")
+    suspend fun findTop10VolumeStocks() {
+        val stockList = stockInformationRepository.findTop10VolumeStocks()
+        val stockLogList = StockLog.from(stockList) // TODO: 비동기에 맞춰서 로직 변경 해야 함
+        stockInformationRepository.saveTop10VolumeStocks(stockLogList) // TODO: save 하는 로직을 event로 떨궈도 될듯?
     }
 }
