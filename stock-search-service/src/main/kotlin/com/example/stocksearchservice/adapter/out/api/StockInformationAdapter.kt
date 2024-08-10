132472754c63605ba73e71141df87a1416f24101
RefinedStone <chem.en.9273@gmail.com>
2024-08-10 14:50:08 +0900

    feat: stockinformationadapter

--- stock-search-service/src/main/kotlin/com/example/stocksearchservice/adapter/out/api/StockInformationAdapter.kt
+++ stock-search-service/src/main/kotlin/com/example/stocksearchservice/adapter/out/api/StockInformationAdapter.kt
@@ -1,15 +1,18 @@
 package com.example.stocksearchservice.adapter.out.api
 
 import com.example.common.ExternalApiAdapter
+import com.example.stocksearchservice.adapter.out.GetCurrentTop20StocksByTradingVolumeHandler
 import com.example.stocksearchservice.application.port.out.StockInformationPort
 import com.example.stocksearchservice.application.port.out.dto.SimpleStockDTO
 
 @ExternalApiAdapter
-class StockInformationAdapter : StockInformationPort {
+internal class StockInformationAdapter(
+    private val getCurrentTop20StocksByTradingVolumeHandler: GetCurrentTop20StocksByTradingVolumeHandler,
+) : StockInformationPort {
     /** TODO: stock의 information을 가져오는 service는 다양할 수 있어서,
      * 정확히 증권사 open-api를 사용해서 데이터를 가져오는 서비스임을 알리는 네이밍이 필요하다. */
     override fun getCurrentTop20StocksByTradingVolume(): List<SimpleStockDTO> {
-        return emptyList() // TODO: 실제 구현 필요
+        return getCurrentTop20StocksByTradingVolumeHandler.execute()
     }
 
     override fun getStockVolumeDuring7days(id: Int): List<SimpleStockDTO> {
