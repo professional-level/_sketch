845619c53ec1449a6bf09298d1ac2b21809f3a89
RefinedStone <chem.en.9273@gmail.com>
2024-08-13 00:04:25 +0900

    feat: stock suggestion repository

--- stock-search-service/src/main/kotlin/com/example/stocksearchservice/adapter/out/persistence/repository/StockSuggestionRepository.kt
+++ stock-search-service/src/main/kotlin/com/example/stocksearchservice/adapter/out/persistence/repository/StockSuggestionRepository.kt
@@ -1,10 +1,12 @@
-package com.example.stocksearchservice.adapter.out.persistence.repository
+//package com.example.stocksearchservice.adapter.out.persistence.repository
+//
+//import com.example.stocksearchservice.adapter.out.persistence.entity.StockVolumeRank
+//import common.AbstractReactiveRepository
+//import jakarta.enterprise.context.ApplicationScoped
+//import org.springframework.stereotype.Repository
+//
+//@ApplicationScoped
+//@Repository
+//class StockSuggestionRepository : AbstractReactiveRepository<StockVolumeRank, Long>()
 
-import com.example.stocksearchservice.adapter.out.persistence.entity.StockSuggestion
-import common.AbstractReactiveRepository
-import jakarta.enterprise.context.ApplicationScoped
-import org.springframework.stereotype.Repository
-
-@ApplicationScoped
-@Repository
-class StockSuggestionRepository : AbstractReactiveRepository<StockSuggestion, Long>()
+// TODO: 파일의 분리가 필요하다면 분리, 하지만 ReactiveStockRepository에서 종합적으로 관리 중
