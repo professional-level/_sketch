3f083999ac69b02a225a3588f586e9442ec8c772
RefinedStone <chem.en.9273@gmail.com>
2024-08-17 03:24:25 +0900

    feat: stock volume rank dto

---
+++ stock-search-service/src/main/kotlin/com/example/stocksearchservice/application/port/out/dto/StockVolumeRankDTO.kt
@@ -0,0 +1,31 @@
+package com.example.stocksearchservice.application.port.out.dto
+
+import com.example.stocksearchservice.domain.Stock
+import com.example.stocksearchservice.domain.StockLog
+import java.time.ZonedDateTime
+
+data class StockVolumeRankDTO(
+    val stockId: Int, // 주식코드
+    val stockPrice: Int, // 주식 현재가격
+    val stockDerivative: Double, // 주식 증감율
+    val stockVolume: Long, // 주식 거래 대금
+    val dateTime: ZonedDateTime, // 해당 날짜
+    val rank: Int, // 순위
+) {
+    fun toStock(): Stock {
+        return Stock.default()
+    }
+
+    companion object {
+        fun fromStock(log: StockLog): StockVolumeRankDTO {
+            return StockVolumeRankDTO(
+                stockId = log.stock.stockId.value,
+                stockPrice = log.stock.stockPrice.value,
+                stockDerivative = log.stock.stockDerivative.value,
+                stockVolume = log.stock.stockVolume.value,
+                dateTime = requireNotNull(log.stockLogInfo).dateTime,
+                rank = requireNotNull(log.stockLogInfo).rank
+            )
+        }
+    }
+}
