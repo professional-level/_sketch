00eefa3ab45c5e532dd379c685668ef247565fbf
RefinedStone <chem.en.9273@gmail.com>
2024-08-31 01:41:34 +0900

    feat: scheduler of final price bating strategy v1

--- stock-search-service/src/main/kotlin/com/example/stocksearchservice/application/scheduler/StockSearchScheduler.kt
+++ stock-search-service/src/main/kotlin/com/example/stocksearchservice/application/scheduler/StockSearchScheduler.kt
@@ -2,6 +2,7 @@ package com.example.stocksearchservice.application.scheduler
 
 import com.example.stocksearchservice.domain.StockLog
 import com.example.stocksearchservice.domain.repository.StockInformationRepository
+import com.example.stocksearchservice.domain.strategy.FinalPriceBatingStrategyV1
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.ZonedDateTime
@@ -25,5 +26,15 @@ class StockSearchScheduler(
         println(stockList)
         val stockLogList = StockLog.from(stockList) // TODO: 비동기에 맞춰서 로직 변경 해야 함
         stockInformationRepository.saveTop10VolumeStocks(stockLogList) // TODO: save 하는 로직을 event로 떨궈도 될듯?
+    }
+
+    // TODO: 해당 알고리즘을 관리하는것을 어떻게 해야 할지 고민이 필요.
+    @Scheduled(cron = "0 50 15 ? * MON-FRI")
+    suspend fun finalPriceBatingStrategy1() {
+        // TODO: 휴장일인지 체크하는 로직이 필요
+        val top10VolumeStockList = stockInformationRepository.findTop10VolumeStocks()
+
+        top10VolumeStockList.filter { FinalPriceBatingStrategyV1.of(stock = it).isValidCurrentValue() }
+        
     }
 }
