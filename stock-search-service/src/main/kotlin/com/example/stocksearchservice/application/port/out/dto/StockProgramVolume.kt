dfbde855628cc369603093eb8ea213c0911a5f5c
RefinedStone <chem.en.9273@gmail.com>
2024-09-14 06:52:58 +0900

    fix: StockProgramVolume dto

--- stock-search-service/src/main/kotlin/com/example/stocksearchservice/application/port/out/dto/StockProgramVolume.kt
+++ stock-search-service/src/main/kotlin/com/example/stocksearchservice/application/port/out/dto/StockProgramVolume.kt
@@ -6,4 +6,5 @@ data class StockProgramVolume(
     val stockDerivative: Double, // 주식 증감율
     val stockVolume: Long, // 주식 거래 대금
     val programVolume: Long, // 프로그램 거래대금
+    val date: String, //ex) 20240814
 )
