30d93e8ec25a17978817706175e6acffcba0762f
RefinedStone <chem.en.9273@gmail.com>
2024-10-11 06:21:37 +0900

    feat: SupportedEventEntityRepository and StockStrategyRepository

--- stock-search-service/src/main/kotlin/com/example/stocksearchservice/domain/repository/FinalPriceBatingStrategyV1Repository.kt
+++ stock-search-service/src/main/kotlin/com/example/stocksearchservice/domain/repository/FinalPriceBatingStrategyV1Repository.kt
@@ -1,5 +1,6 @@
 package com.example.stocksearchservice.domain.repository
 
+import com.example.stocksearchservice.domain.event.EventSupportedEntity
 import com.example.stocksearchservice.domain.strategy.FinalPriceBatingStrategyV1
 import com.example.stocksearchservice.domain.strategy.StockStrategy
 import java.time.ZonedDateTime
@@ -7,7 +8,10 @@ import java.time.ZonedDateTime
 interface FinalPriceBatingStrategyV1Repository : StockStrategyRepository<FinalPriceBatingStrategyV1>
 
 // TODO: 공통 인터페이스 위치 이동 필요
-interface StockStrategyRepository<T : StockStrategy> {
+interface StockStrategyRepository<T : StockStrategy> : SupportedEventEntityRepository<StockStrategy> {
     suspend fun save(entity: T, date: ZonedDateTime = ZonedDateTime.now())
     suspend fun saveAll(entities: List<T>, date: ZonedDateTime = ZonedDateTime.now())
 }
+
+// 공통 인터페이스
+interface SupportedEventEntityRepository<T : EventSupportedEntity>
