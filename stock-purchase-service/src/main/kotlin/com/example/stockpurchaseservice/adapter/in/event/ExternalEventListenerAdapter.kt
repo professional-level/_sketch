9ac39c989a64b96a234215d43e116f0bff4e1905
RefinedStone <chem.en.9273@gmail.com>
2024-10-26 15:56:54 +0900

    feat: protobuff event apply

--- stock-purchase-service/src/main/kotlin/com/example/stockpurchaseservice/adapter/in/event/ExternalEventListenerAdapter.kt
+++ stock-purchase-service/src/main/kotlin/com/example/stockpurchaseservice/adapter/in/event/ExternalEventListenerAdapter.kt
@@ -1,38 +1,82 @@
 package com.example.com.example.stockpurchaseservice.adapter.`in`.event
 
+import Event
 import com.example.common.ExternalApiAdapter
+import com.google.protobuf.Timestamp
+import common.Topic.STRATEGY_SAVED
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 @ExternalApiAdapter // TODO: 정확한 annotation 매칭
 internal class ExternalEventListenerAdapter(
-    private val buyingStockPurchaseUseCase: BuyingStockPurchaseUseCase
+    private val buyingStockPurchaseUseCase: BuyingStockPurchaseUseCase,
 ) {
-    @KafkaListener(topics = ["strategies-saved-topic"]) // TODO: topic enum으로 관리하며 common으로 이동 필요
+    @KafkaListener(topics = [STRATEGY_SAVED]) // TODO: topic enum으로 관리하며 common으로 이동 필요
     fun createdStrategies(message: String) {
         // 구현
         println(message)
-        buyingStockPurchaseUseCase.execute(BuyingStockPurchaseRequest("temp"))
+        val tempMessage: ByteArray = byteArrayOf()
+        val event = Event.StrategiesSavedEvent.parseFrom(tempMessage)
+        buyingStockPurchaseUseCase.execute(event.toCommand())
     }
 }
 
-typealias BuyingStockPurchaseUseCase = StockPurchaseUseCase<BuyingStockPurchaseRequest,BuyingStockPurchaseResult>
+interface BuyingStockPurchaseUseCase : StockPurchaseUseCase<BuyingStockPurchaseCommand, BuyingStockPurchaseResult>
 interface StockPurchaseUseCase<T, R> : UseCase<T, R>
 
-interface UseCase<T,R> {
-    fun execute(request: T) : R
+// TODO: 사용해 보고 큰 문제 없으면 common으로 이동
+interface UseCase<T, R> {
+    fun execute(request: T): R
 }
 
 @Service
-class BuyingStockPurchaseService: BuyingStockPurchaseUseCase{
-    override fun execute(request: BuyingStockPurchaseRequest): BuyingStockPurchaseResult {
+class BuyingStockPurchaseService : BuyingStockPurchaseUseCase {
+    override fun execute(request: BuyingStockPurchaseCommand): BuyingStockPurchaseResult {
         TODO("Not yet implemented")
     }
 }
+fun Event.StrategiesSavedEvent.toCommand(): BuyingStockPurchaseCommand {
+   return BuyingStockPurchaseCommand(
+        stockId = stockId,
+        requestAt = savedAt.toZonedDateTime(),
+        type = type.convert()
+    )
+}
 
-data class BuyingStockPurchaseRequest(
-    val temp: String
-)
 data class BuyingStockPurchaseResult(
-    val temp: String
+    val temp: String,
 )
+
+class BuyingStockPurchaseCommand(
+    stockId: String,
+    val requestAt: ZonedDateTime,
+    val type: StrategyType,
+) {
+    val stockId: StockId = StockId(stockId)
+}
+
+@JvmInline
+value class StockId(val value: String)
+
+enum class StrategyType {
+    Undefined,
+    FinalPriceBatingV1
+}
+
+// 확장함수
+fun Event.StrategyType.convert(): StrategyType {
+    return when (this) {
+        Event.StrategyType.UNDEFINED -> StrategyType.Undefined // TODO: UNRECOGNIZED 사용하도록 변경 필요
+        Event.StrategyType.FINAL_PRICE_BATING_V1 -> StrategyType.FinalPriceBatingV1
+        Event.StrategyType.UNRECOGNIZED -> StrategyType.Undefined
+    }
+}
+
+// protobuff 용 timestamp 값을 convert
+fun Timestamp.toZonedDateTime(zone: ZoneId = ZoneId.systemDefault()): ZonedDateTime {
+    val instant = Instant.ofEpochSecond(this.seconds, this.nanos.toLong())
+    return ZonedDateTime.ofInstant(instant, zone)
+}
--- stock-service/src/main/kotlin/com/example/stock/application/port/out/AssembleStockInfoPort.kt
+++ stock-service/src/main/kotlin/com/example/stock/application/port/out/AssembleStockInfoPort.kt
@@ -3,7 +3,6 @@ package com.example.stock.application.port.out
 import com.example.stock.application.port.out.dto.StockDTO
 
 interface AssembleStockInfoPort {
-
     fun assembleStockInfo(
         stockId: Int,
         stockName: String,
