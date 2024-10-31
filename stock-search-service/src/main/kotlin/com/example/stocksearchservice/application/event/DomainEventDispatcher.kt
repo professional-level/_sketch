c2b9b04ef1c76135f952c18f5b8622b13dd720b5
RefinedStone <chem.en.9273@gmail.com>
2024-11-01 03:56:43 +0900

    fix: DomainEventDispatcher for proto type message

---
+++ stock-search-service/src/main/kotlin/com/example/stocksearchservice/application/event/DomainEventDispatcher.kt
@@ -0,0 +1,32 @@
+package com.example.stocksearchservice.application.event
+
+import com.example.stocksearchservice.application.port.out.message.MessageServicePort
+import com.example.stocksearchservice.domain.event.DomainEvent
+import com.example.stocksearchservice.domain.event.StrategyCreatedEvent
+import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.springframework.stereotype.Component
+
+@Component
+class DomainEventDispatcher(
+    private val messageServicePort: MessageServicePort,
+    private val objectMapper: ObjectMapper,
+    private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.IO),
+    private val eventMapper: StockSearchServiceEventMapper,
+) {
+    fun dispatch(events: List<DomainEvent>) {
+        /*TODO: 추후 비동기 로직 적용 필요*/
+        coroutineScope.launch {
+            events.forEach { event ->
+                val eventMessage = eventMapper.map(event)
+                messageServicePort.publish(eventMessage)
+            }
+        }
+    }
+}
+
+private fun StrategyCreatedEvent.toMessage() {
+    return TODO()
+}
