49f4f944c430b43ecd05ed97b5e1e1504f7869b7
RefinedStone <chem.en.9273@gmail.com>
2024-11-02 02:55:41 +0900

    feat: StockSearchServiceEventMapper

---
+++ stock-search-service/src/main/kotlin/com/example/stocksearchservice/application/event/StockSearchServiceEventMapper.kt
@@ -0,0 +1,39 @@
+package com.example.stocksearchservice.application.event
+
+import com.example.common.application.event.ApplicationEvent
+import com.example.common.application.event.EventMapper
+import com.example.common.application.event.EventMessage
+import com.example.stocksearchservice.domain.event.DomainEvent
+import com.example.stocksearchservice.domain.event.StrategyCreatedEvent
+import common.MessageTopic
+import org.springframework.stereotype.Component
+import kotlin.reflect.KClass
+
+
+@Component
+class StockSearchServiceEventMapper : EventMapper<DomainEvent, ApplicationEvent>() {
+    override val functionMapper: Map<KClass<out DomainEvent>, (DomainEvent) -> EventMessage<ApplicationEvent>> = mapOf(
+        StrategyCreatedEvent::class to { event ->
+            EventMessage(
+                convertedEvent = mapStrategyCreatedEvent(event as StrategyCreatedEvent),
+                messageTopic = MessageTopic.STRATEGY_SAVED,
+            )
+        }
+        // 다른 도메인 이벤트 매핑 추가
+    )
+
+    override fun map(event: DomainEvent): EventMessage<ApplicationEvent> {
+        return functionMapper[event::class]?.invoke(event)
+            ?: throw IllegalArgumentException("Unsupported event type: ${event::class}")
+    }
+
+    private fun mapStrategyCreatedEvent(event: StrategyCreatedEvent): StrategyCreatedApplicationEvent {
+        return StrategyCreatedApplicationEvent(
+            stockId = event.stockId,
+            type = StrategyTypeDto.from(event.type),
+            savedAt = event.savedAt,
+            id = event.id,
+            occurredAt = event.occurredAt,
+        )
+    }
+}
