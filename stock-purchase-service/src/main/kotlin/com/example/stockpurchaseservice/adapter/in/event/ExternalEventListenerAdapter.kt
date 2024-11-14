93672afe1a73b533a6e7a65657ea0a41cb146585
RefinedStone <chem.en.9273@gmail.com>
2024-11-14 18:46:07 +0900

    fix: ExternalEventListenerAdapter with abstract handler mapper

--- stock-purchase-service/src/main/kotlin/com/example/stockpurchaseservice/adapter/in/event/ExternalEventListenerAdapter.kt
+++ stock-purchase-service/src/main/kotlin/com/example/stockpurchaseservice/adapter/in/event/ExternalEventListenerAdapter.kt
@@ -1,73 +1,28 @@
 package com.example.com.example.stockpurchaseservice.adapter.`in`.event
 
 import Event
+import com.example.com.example.stockpurchaseservice.application.port.`in`.BuyingStockPurchaseUseCase
+import com.example.com.example.stockpurchaseservice.application.service.BuyingStockPurchaseCommand
+import com.example.com.example.stockpurchaseservice.application.service.StockId
+import com.example.com.example.stockpurchaseservice.application.service.StrategyType
 import com.example.common.ExternalApiAdapter
+import common.ConsumerGroupId.PURCHASE_SERVICE
 import common.Topic.STRATEGY_SAVED
+import common.proto.ProtoUtils.toZonedDateTime
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.stereotype.Service
-import java.time.ZonedDateTime
 
 @ExternalApiAdapter // TODO: 정확한 annotation 매칭
 internal class ExternalEventListenerAdapter(
-    private val buyingStockPurchaseUseCase: BuyingStockPurchaseUseCase,
+    private val buyingStockPurchaseUseCase: BuyingStockPurchaseUseCase, // TODO: ServiceMapper class 필요
 ) {
-    @KafkaListener(topics = [STRATEGY_SAVED]) // TODO: topic enum으로 관리하며 common으로 이동 필요
-    fun createdStrategies(message: String) {
+    @KafkaListener(topics = [STRATEGY_SAVED], groupId = PURCHASE_SERVICE) // TODO: topic enum으로 관리하며 common으로 이동 필요
+    fun createdStrategies(message: ByteArray ) {
         // 구현
-        println(message)
+        println(message) // TODO: 테스트 후 제거
         val tempMessage: ByteArray = byteArrayOf()
-        val event = Event.StrategiesSavedEvent.parseFrom(tempMessage)
-//        buyingStockPurchaseUseCase.execute(event.toCommand())
+        val event = Event.StrategySavedEvent.parseFrom(tempMessage)
+        buyingStockPurchaseUseCase.execute(event.toCommand())
     }
-}
-
-interface BuyingStockPurchaseUseCase : StockPurchaseUseCase<BuyingStockPurchaseCommand, BuyingStockPurchaseResult>
-interface StockPurchaseUseCase<T, R> : UseCase<T, R>
-
-// TODO: 사용해 보고 큰 문제 없으면 common으로 이동
-interface UseCase<T, R> {
-    fun execute(request: T): R
-}
-
-@Service
-class BuyingStockPurchaseService : BuyingStockPurchaseUseCase {
-    override fun execute(request: BuyingStockPurchaseCommand): BuyingStockPurchaseResult {
-        val stockId = request.stockId
-        val requestAt = request.requestAt
-        val requestType = request.type // TODO: typeMapper, mapper 공통기능 만들필요 있을 듯
-        val stockInfo = stockInfoRepository.findByStockId(stockId)
-
-        // TODO: BuyingStockPurchaseService가 아니라 전략별로 Domain 로직을 만들어, 상세조건, 구매 가격, 익절,손절라인 등 지정필요
-        return TODO()
-    }
-}
-
-//fun Event.StrategiesSavedEvent.toCommand(): BuyingStockPurchaseCommand {
-//    return BuyingStockPurchaseCommand(
-//        stockId = stockId,
-//        requestAt = savedAt.toZonedDateTime(),
-//        type = type.convert(),
-//    )
-//}
-
-data class BuyingStockPurchaseResult(
-    val temp: String,
-)
-
-class BuyingStockPurchaseCommand(
-    stockId: String,
-    val requestAt: ZonedDateTime,
-    val type: StrategyType,
-) {
-    val stockId: StockId = StockId(stockId)
-}
-
-@JvmInline
-value class StockId(val value: String)
-
-enum class StrategyType {
-    Undefined,
-    FinalPriceBatingV1,
 }
 
 // 확장함수
@@ -76,5 +31,21 @@ fun Event.StrategyType.convert(): StrategyType {
         Event.StrategyType.UNDEFINED -> StrategyType.Undefined // TODO: UNRECOGNIZED 사용하도록 변경 필요
         Event.StrategyType.FINAL_PRICE_BATING_V1 -> StrategyType.FinalPriceBatingV1
         Event.StrategyType.UNRECOGNIZED -> StrategyType.Undefined
+    }
+}
+
+fun Event.StrategySavedEvent.toCommand(): BuyingStockPurchaseCommand {
+    val event = this
+    val strategyType = event.type.convert()
+    return when (strategyType) {
+        StrategyType.FinalPriceBatingV1 -> BuyingStockPurchaseCommand.OfFinalPriceBatingV1(
+            stockId = StockId(event.stockId),
+            stockName = event.stockName,
+            requestAt = event.savedAt.toZonedDateTime(),
+            type = strategyType,
+            purchasePrice = 0.0 // TODO: 실제 구매 가격으로 변경 필요
+        )
+
+        else -> throw IllegalArgumentException("지원하지 않는 전략 타입입니다: $strategyType")
     }
 }
