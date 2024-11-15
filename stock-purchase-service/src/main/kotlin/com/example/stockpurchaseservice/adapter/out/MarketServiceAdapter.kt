f7b5ec0da43de0c79b4bcbc5507debe012be93c3
RefinedStone <chem.en.9273@gmail.com>
2024-11-15 09:23:32 +0900

    feat: MarketService port and adapter

---
+++ stock-purchase-service/src/main/kotlin/com/example/stockpurchaseservice/adapter/out/MarketServiceAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.com.example.stockpurchaseservice.adapter.out
+
+import com.example.com.example.stockpurchaseservice.application.port.out.MarketServicePort
+import com.example.com.example.stockpurchaseservice.application.port.out.PurchaseOrderDto
+import com.example.common.ExternalApiAdapter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+
+@ExternalApiAdapter // TODO: 어노테이션 체크
+internal class MarketServiceAdapter(
+    private val buyStockHandler: BuyStockHandler,
+) : MarketServicePort {
+    override fun buyStock(order: PurchaseOrderDto) {
+        buyStockHandler.execute()
+    }
+}
+
+@Component
+internal class BuyStockHandler(
+    private val stockApiClient: WebClient,
+) {
+    fun execute() {
+        val uri = "temp_uri"
+        val response = stockApiClient
+            .get()
+            .uri(uri)
+            .accept(MediaType.APPLICATION_PROTOBUF)
+            .retrieve()
+            .toEntity(ResponseEntity::class.java) // TODO: map to ProtoType Entity
+            .block()
+        // TODO: 유효성 체크
+        response?.body?.body
+    }
+}
+
+
+@Configuration
+class WebClientConfig {
+    @Bean
+    fun stockApiClient(): WebClient {
+        val port = 8079
+        // TODO: uri 설정을 추후 제대로 해야 함
+        return WebClient.create("http://localhost:$port")
+    }
+}
--- stock-purchase-service/src/main/kotlin/com/example/stockpurchaseservice/application/port/out/MarketServicePort.kt
+++ stock-purchase-service/src/main/kotlin/com/example/stockpurchaseservice/application/port/out/MarketServicePort.kt
@@ -1,3 +1,13 @@
 package com.example.com.example.stockpurchaseservice.application.port.out
 
-interface MarketServicePort
+import java.util.UUID
+
+interface MarketServicePort {
+    fun buyStock(order: PurchaseOrderDto)
+}
+
+data class PurchaseOrderDto(
+    val orderId: UUID,
+    val stockId: String,
+    val purchasePrice: Double,
+)
