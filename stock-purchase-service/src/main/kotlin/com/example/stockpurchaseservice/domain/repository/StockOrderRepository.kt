0dde20506d480f24d6f6f507e6e3df117823b7d9
RefinedStone <chem.en.9273@gmail.com>
2024-11-25 10:32:20 +0900

    feat: StockOrderRepository in purchase-service module

---
+++ stock-purchase-service/src/main/kotlin/com/example/stockpurchaseservice/domain/repository/StockOrderRepository.kt
@@ -0,0 +1,8 @@
+package com.example.com.example.stockpurchaseservice.domain.repository
+
+import com.example.common.DomainRepository
+import com.example.stockpurchaseservice.domain.Order
+import com.example.stockpurchaseservice.domain.OrderId
+
+
+interface StockOrderRepository: DomainRepository<Order, OrderId>
