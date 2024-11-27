52fc9caa042aff35c3047866e272da7639f0778a
RefinedStone <chem.en.9273@gmail.com>
2024-11-27 14:12:50 +0900

    wip: EventListener

---
+++ stock-purchase-service/src/main/kotlin/com/example/stockpurchaseservice/application/event/EventListener.kt
@@ -0,0 +1,17 @@
+package com.example.com.example.stockpurchaseservice.application.event
+
+import com.example.com.example.stockpurchaseservice.domain.repository.StockOrderRepository
+import com.example.common.domain.event.DomainEvent
+import com.example.stockpurchaseservice.domain.SellingSuccessEvent
+
+class EventListener(
+    private val stockOrderRepository: StockOrderRepository,
+) {
+
+    fun listenCreatedOrder(event: DomainEvent) {
+        event as SellingSuccessEvent
+
+    }
+    fun listenUpdatedOrder(){}
+
+}
